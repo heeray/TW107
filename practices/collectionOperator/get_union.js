25785783d87ece008adc3d349c163b706b847b5f
heeray <hhxxdnp@163.com>
2017-11-05 15:47:34 +0800

    get_unions commit

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -2,7 +2,16 @@
 
 function get_union(collection_a, collection_b) {
   //在这里写入代码
-}
+  var a = collection_a.concat(collection_b);
+      for(var i=0; i<a.length; ++i) {
+          for(var j=i+1; j<a.length; ++j) {
+              if(a[i] === a[j])
+                  a.splice(j--, 1);
+          }
+      }
+
+     return a;
+ }
+
 
 module.exports = get_union;
-
