b6e8c313a6066185239de7c626e72c2d2d5bc028
heeray <hhxxdnp@163.com>
2017-11-05 12:39:16 +0800

    get_letter_interval commit

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -2,6 +2,28 @@
 
 function get_letter_interval(number_a, number_b) {
   //在这里写入代码
+  var arr=[];
+  if(number_a<number_b)
+  {
+    for(var i = number_a;i<=number_b;i++)
+    {
+        arr.push(String.fromCharCode(i+96));
+    }
+    return arr;
+  }
+  else if(number_a>number_b)
+  {
+    for(var i = number_a;i>=number_b;i--)
+    {
+        arr.push(String.fromCharCode(i+96));
+    }
+    return arr;
+  }
+  else if(number_a==number_b)
+  {
+     arr.push(String.fromCharCode(number_a+96));
+    return arr;
+  }
 }
 
 module.exports = get_letter_interval;
