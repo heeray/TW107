ea4eb760195ca7e54348b42988fd273b15163176
heeray <hhxxdnp@163.com>
2017-11-05 12:03:04 +0800

    get_interger_interval commit

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -2,6 +2,28 @@
 
 function get_integer_interval(number_a, number_b) {
   //在这里写入代码
+  let arr=[];
+  if(number_a<number_b)
+  {
+    for(var i = number_a;i<=number_b;i++)
+    {
+        arr.push(i);
+    }
+    return arr;
+  }
+  else if(number_a>number_b)
+  {
+    for(var i = number_a;i>=number_b;i--)
+    {
+        arr.push(i);
+    }
+    return arr;
+  }
+  else if(number_a==number_b)
+  {
+    arr.push(number_a);
+    return arr;
+  }
 }
 
 module.exports = get_integer_interval;
