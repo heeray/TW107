eaacce186257ce55f40e3bdf2dbb9b91ec6841f2
heeray <hhxxdnp@163.com>
2017-11-05 12:10:21 +0800

    get_intersection commit

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -2,6 +2,12 @@
 
 function get_intersection(collection_a, collection_b) {
   //在这里写入代码
+  let arr=[];
+	collection_b.forEach((a)=>{
+		if(collection_a.indexOf(a)!=-1)
+			arr.push(a);
+	});
+  return arr;
 }
 
 module.exports = get_intersection;
