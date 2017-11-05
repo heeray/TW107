dff3761d1f928801d47b557e34494527500c260a
heeray <hhxxdnp@163.com>
2017-11-05 15:00:36 +0800

    get_letter_interval_2 commit

--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -2,6 +2,74 @@
 
 function get_letter_interval_2(number_a, number_b) {
   //在这里写入代码
+  var arr=[];
+
+  if(number_a<number_b)
+  {
+    for(var i =number_a;i<=number_b;i++)
+    {
+      if(i>26)
+      {
+        var n1=i%26;
+        var n2=parseInt(i/26);
+        if(n1==0)
+        {
+           arr.push(String.fromCharCode(n2-1+96)+String.fromCharCode(26+96));
+        }
+        else
+        {
+          arr.push(String.fromCharCode(n2+96)+String.fromCharCode(n1+96));
+        }        
+      }
+      else{
+         arr.push(String.fromCharCode(i+96));
+      }
+    }
+    return arr;
+  }
+  else if(number_a>number_b)
+  {
+    for(var i = number_a;i>=number_b;i--)
+    {
+        if(i>26)
+      {
+        var n1=i%26;
+        var n2=parseInt(i/26);
+        if(n1==0)
+        {
+           arr.push(String.fromCharCode(n2-1+96)+String.fromCharCode(26+96));
+        }
+        else
+        {
+          arr.push(String.fromCharCode(n2+96)+String.fromCharCode(n1+96));
+        }        
+      }
+      else{
+         arr.push(String.fromCharCode(i+96));
+    }
+    }
+    return arr;
+  }
+  else if(number_a==number_b)
+  {
+     if(number_a>26)
+      {
+        var n1=number_a%26;
+        var n2=parseInt(number_a/26);
+        if(n1==0)
+        {
+           arr.push(String.fromCharCode(n2-1+96)+String.fromCharCode(26+96));
+        }
+        else
+        {
+          arr.push(String.fromCharCode(n2+96)+String.fromCharCode(n1+96));
+        }        
+      }
+      else{
+         arr.push(String.fromCharCode(number_a+96));
+    }
+    return arr;
+  }
 }
 
 module.exports = get_letter_interval_2;
